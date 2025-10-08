131bec19953be8c952685e0a37da3da30911ed9f
Leonardo Silva <141833471+leeo-s@users.noreply.github.com>
2025-10-08 19:49:54 -0300

    Removendo todos os arquivos

--- src/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import * as userController from "../controllers/userController.js";
-
-router.post("/cadastrar", userController.cadastrar);
-
-export default router;
