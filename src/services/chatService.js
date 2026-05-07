f69e5e6e4631a2ae2b06296951d9fe930727bf39
Leonardo Silva <leo.bs1997@hotmail.com>
2026-05-07 16:29:16 -0300

    ajustes gemini

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,10 +1,7 @@
-import { PrismaClient } from '../generated/prisma/index.js';
-//import prisma from '../config/prisma.js';
+import prisma from '../config/prisma.js';
 import CustomError from '../utils/CustomError.js';
 import { authenticateUser } from './userService.js';
 import { getPageNumber } from '../utils/validations.js';
-
-const prisma = new PrismaClient();
 
 const actions = {
   startChat: 'iniciar chat',
